829c89f50d70977c3a0869c9a1d0d8afc7a8024c
garlapatirahul <rahulreddy1991@gmail.com>
2016-03-20 20:21:30 -0700

    Update apb_sequences.svh

--- apb_sequences.svh
+++ apb_sequences.svh
@@ -18,10 +18,12 @@ class apb_base_seq extends uvm_sequence#(apb_rw);
 apb_rw apb_rw_item;
   //Main Body method that gets executed once sequence is started
   task body();
+    repeat(10) begin
     apb_rw_item = apb_rw::type_id::create("apb_rw_item");
     start_item(apb_rw_item);
     if(!item.randomize() with {address inside [0:32'h4FFF_FFFF]};}) begin
       `uvm_error("body", "randomizaton failure for item")
+    end
     end
     finish_item(apb_rw_item);
     
